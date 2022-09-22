00973f247c87bdc9bdbae6898fdbe65ca12cd566
032605 <032605@naver.com>
2022-09-22 23:13:17 +0900

    Changed Discount Policy

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            // 실제 실무에서는 테스트가 어마어마. 경계값 테스트 등
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
--- src/main/java/hello/core/order/OrderServiceimpl.java
+++ src/main/java/hello/core/order/OrderServiceimpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -9,8 +10,9 @@ import hello.core.member.MemoryMemberRepositroy;
 
 public class OrderServiceimpl implements OrderService{
     private MemberRepository memberRepository = new MemoryMemberRepositroy();
-    private DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+    //private DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
