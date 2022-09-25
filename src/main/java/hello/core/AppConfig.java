2189c367cb684cf6834966018ae055c53e864dc7
032605 <032605@naver.com>
2022-09-25 23:59:25 +0900

    java to Spring(AppConfig)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,28 @@ import hello.core.member.MemebrServiceImpl;
 import hello.core.member.MemoryMemberRepositroy;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceimpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemebrServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepositroy memberRepository() {
+    @Bean
+    public static MemoryMemberRepositroy memberRepository() {
         return new MemoryMemberRepositroy();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceimpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new RateDiscountPolicy();
         return new FixDiscountPolicy();
