1df3ee8ff1319eef5fe2c5deabc635c063eae2dd
032605 <032605@naver.com>
2022-10-28 22:01:54 +0900

    Provider 사용하여 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,8 +2,8 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -41,18 +41,12 @@ public class SingletonWithPrototypeTest1 {
 
     @Scope("singleton")
     static class ClientBean{
-        //private final PrototypeBean prototypeBean;  // 생성시점에 주입
 
         @Autowired
-        ApplicationContext applicationContext;
-
-        /*@Autowired
-        ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }*/
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;    // 테스트니깐 간단하게 필드주입
 
         public int logic() {
-            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();    // 찾아주는 기능만 제공
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
