838e9cabbd2dae27237540302e059bf95bf4d0cf
032605 <032605@naver.com>
2022-10-28 21:39:36 +0900

    프로토타입과 싱글톤을 같이 사용했을 때의 문제점을 야매로 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,6 +3,7 @@ package hello.core.scope;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -34,20 +35,24 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count2 = clientBean.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;  // 생성시점에 주입
+        //private final PrototypeBean prototypeBean;  // 생성시점에 주입
 
         @Autowired
+        ApplicationContext applicationContext;
+
+        /*@Autowired
         ClientBean(PrototypeBean prototypeBean) {
             this.prototypeBean = prototypeBean;
-        }
+        }*/
 
         public int logic() {
+            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
