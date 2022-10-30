8210b67ddcafca87dd9ea564b5845281a8471f1b
032605 <032605@naver.com>
2022-10-30 22:03:26 +0900

    web scope (request scope)

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,35 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.security.PublicKey;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;   // 빈 생성 시점을 알 수 없어 값이 외부에서 들어오도록 함
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init(){
+        String uuid = UUID.randomUUID().toString(); // 로또 맞을 확률보다 더더더 낮다
+        System.out.println("[" + uuid + "] request scope bean create : " + this);
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("[" + uuid + "] request scope bean close : " + this);
+    }
+}
